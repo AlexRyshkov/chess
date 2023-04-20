1354521ee004fc5712f944f0a9a7f4d56c6da2a1
AlexRyshkov <alexryshkov1000.7@gmail.com>
2023-04-20 21:34:16 +0300

    setup server

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './src/index.ts',
+    experiments: {
+        topLevelAwait: true,
+    },
+    externals: [nodeExternals()],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: { type: 'commonjs2' },
+    },
+    plugins: [
+        new NodemonPlugin(),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    target: 'node',
+};
