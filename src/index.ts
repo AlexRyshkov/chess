8280f14c6153026f9ba716ce52b74cfa9398b1c0
AlexRyshkov <alexryshkov72@gmail.com>
2023-04-23 01:12:15 +0300

    add objection js

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import router from "src/routes";
+import config from 'src/knexfile'
 const app = express();
 const port = 3002;
 
 
+const { Model } = require('objection');
+const Knex = require('knex');
+
+const knex = Knex(config.development);
+
+Model.knex(knex);
+
+
 app.use(router)
+
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
