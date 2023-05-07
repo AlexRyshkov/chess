106b0a7130e033e937447c272c1fa5bf58714df3
AlexRyshkov <alexryshkov72@gmail.com>
2023-05-07 18:58:24 +0300

    add .env file support for development

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,33 +1,35 @@
-const path = require('path');
-const NodemonPlugin = require('nodemon-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    externals: [nodeExternals()],
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: { type: 'commonjs2' },
-    },
-    plugins: [
-        new NodemonPlugin(),
+  entry: path.resolve(__dirname, "src/index.ts"),
+  externals: [nodeExternals()],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, 'src/') // added this
-        },
-        extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: { type: "commonjs2" },
+  },
+  plugins: [
+    new NodemonPlugin({
+      nodeArgs: ["-r", "dotenv/config"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src/"), // added this
     },
-    target: 'node',
+    extensions: [".ts", ".js"],
+  },
+  target: "node",
 };
